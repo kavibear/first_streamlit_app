d506d156930e231eb647ea23141cbaee3b3b32ca
Kaviana Bear <121263041+kavibear@users.noreply.github.com>
2022-12-22 16:49:11 -0500

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breaksfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smootie')
-streamit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
