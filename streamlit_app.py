9e8f1ef46bb020f8552bd1a2616a20d3f3438f14
Kaviana Bear <121263041+kavibear@users.noreply.github.com>
2022-12-22 16:56:46 -0500

    Update streamlit_app.py
    Add new Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
   
-streamlit.header(' Breaksfast Menu')
+streamlit.header(' Breaksfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smootie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toat')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
