c8ca10103a9670cd3d5680a5606618562380801b
Kaviana Bear <121263041+kavibear@users.noreply.github.com>
2022-12-22 16:48:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breaksfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamit.text('Hard-Boiled Free-Range Egg')
+
