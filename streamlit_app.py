ab1fa5c7b15d4282b428e4de4799b37ac2861dbc
Kaviana Bear <121263041+kavibear@users.noreply.github.com>
2022-12-22 16:55:29 -0500

    Update streamlit_app.py
    Adds icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-
-streamlit.header('Breaksfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smootie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+  
+streamlit.header(' Breaksfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smootie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toat')
