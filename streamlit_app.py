eb282c1232b717fbef1e51028ab61d02d1986d73
Kaviana Bear <121263041+kavibear@users.noreply.github.com>
2022-12-28 01:18:45 -0500

    Update streamlit_app.py
    Add Jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()  #fetchone()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
+streamlit.write('The user entered ', add_my_fruit)
